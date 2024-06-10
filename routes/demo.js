7f2fb6b8bca6de56056c147103fbd71a166b6429
vaibhavkwagh <vaibhavkw2001@gmail.com>
2024-06-10 11:38:56 +0530

    changed the plan id to live in demo

--- routes/demo.js
+++ routes/demo.js
@@ -28,8 +28,8 @@ router.get("/", (req, res) => {
 
 router.post("/subscription", async (req,res) => {
     const options = {
-        // plan_id: "plan_NxhIybqBiYaDx2", // live plan curiotory
-        plan_id: "plan_OJmqrAJKPTySg6", // demo plan curiotory
+        plan_id: "plan_NxhIybqBiYaDx2", // live plan curiotory
+        // plan_id: "plan_OJmqrAJKPTySg6", // demo plan curiotory
         customer_notify: 1,
         total_count: 12, // Total number of billing cycles, for a monthly subscription for a year
     }
