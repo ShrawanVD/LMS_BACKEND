562152bf649b7c63bab2d20c1104de14e0c5574d
ShrawanVD <shra1.deo7714@gmail.com>
2024-07-22 18:36:58 +0530

    amp not working

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import payment1 from './routes/demo.js';
 import course from './routes/course.js';
 import lead from "./routes/leads.js";
 
-import path from 'path';  // Import the path module
+// import path from 'path';  // Import the path module
 
 connectToMongo();
 const app = express()
@@ -16,8 +16,8 @@ const port = 4000
 app.use(express.json());
 app.use(cors());
 
-// Serve static files from the 'public' directory
-app.use(express.static(path.join(__dirname, 'public')));
+// // Serve static files from the 'public' directory
+// app.use(express.static(path.join(__dirname, 'public')));
 
 
 // Available routes    
@@ -25,10 +25,10 @@ app.get('/', (req, res) => {
     res.send('S Buying the course')
 })
 
-// Route to serve the AMP page
-app.get('/amp', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'amp.html'));
-});
+// // Route to serve the AMP page
+// app.get('/amp', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'public', 'amp.html'));
+// });
 
 app.use('/api/payment', payment1)
 app.use('/api',course);
